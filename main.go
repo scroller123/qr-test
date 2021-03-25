e977b3f5e78f18532bbdff166d4874e6e94e6cda
Dmitry Vyatkin <d.vyatkin@corp.mail.ru>
2021-03-25 20:35:56 +0300

    init

--- main.go
+++ main.go
@@ -5,15 +5,14 @@ import (
 	qrcode "github.com/skip2/go-qrcode"
 	"net/http"
 	"os"
-	"time"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	start := time.Now()
-	defer func() {
-		total := time.Now().Sub(start).Microseconds()
-		fmt.Println(total)
-	}()
+	//start := time.Now()
+	//defer func() {
+	//	total := time.Now().Sub(start).Microseconds()
+	//	fmt.Println(total)
+	//}()
 	var png []byte
 	png, err := qrcode.Encode("dclub://pushaction/?data=eyJtZXRob2QiOjYsIm9yZGVyX2hhc2giOiJkZDVkZDdlNTIwM2Y0NDM4M2RjN2M2YzBhNzljMTAwODlkZjkyZjc0In0=", qrcode.Medium, 256)
 	if err != nil {
